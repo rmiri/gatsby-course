13eedae3463bfa9c48adb2458364c673f87b6ba2
rmiri <r.miriuk@tutanota.com>
2020-09-20 19:59:08 +0100

    added a form

---
+++ src/components/form.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { useForm } from "react-hook-form"
+
+export default function WebForm() {
+  const { handleSubmit, register, errors } = useForm()
+  const onSubmit = values => console.log(values)
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label for="email">Email</label>
+      <input
+        name="email"
+        ref={register({
+          required: "Required",
+          pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            message: "invalid email address",
+          },
+        })}
+      />
+      {errors.email && errors.email.message}
+
+      <input
+        name="username"
+        ref={register({
+          validate: value => value !== "admin" || "Nice try!",
+        })}
+      />
+      {errors.username && errors.username.message}
+
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+// export default WebForm;
